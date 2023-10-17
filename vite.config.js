3d404e5e62a8cb02cc3e31c84dc44e8f7c3f3e7e
Minato <erms12.99@hotmail.com>
2023-10-16 18:49:30 -0600

    xd3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://Minatox99.github.io/react-deploy-example/",
+  base: "react-deploy-example",
 });
